59f575924f8d36ae5905646c472b1136a715b893
Holoti <106698219+Holoti@users.noreply.github.com>
2024-02-07 23:11:37 +0500

    feat(frontend): Add and change constants

--- src/constants.py
+++ src/constants.py
@@ -1,13 +1,30 @@
 from enum import Enum, auto
 from dotenv import dotenv_values
-
-
-class State(Enum):
-    START = auto()
-    SELECT_METRIC_NAME = auto()
-    SELECT_METRIC_TIME = auto()
-    SETUP_FINISHED = auto()
+from telegram import ReplyKeyboardMarkup
 
 
 config = dotenv_values("src/.env")
 BOT_TOKEN = config['BOT_TOKEN']
+
+
+class State(Enum):
+    START = auto()
+    SELECT_METRIC_TYPE = auto()
+    SELECT_METRIC_NAME = auto()
+    SELECT_METRIC_TIME = auto()
+
+
+class Button(Enum):
+    CREATE_METRIC = 'Создать Метрику'
+    LIST_METRIC = 'Список Метрик'
+    ADD_ENTRY = 'Добавить запись'
+    SETTINGS = 'Настройки'
+    ABOUT = 'О проекте'
+
+menu_keyboard = [
+    [Button.CREATE_METRIC.value, Button.LIST_METRIC.value],
+    [Button.ADD_ENTRY.value],
+    [Button.SETTINGS.value, Button.ABOUT.value]
+]
+
+menu_keyboard_markup = ReplyKeyboardMarkup(menu_keyboard, input_field_placeholder='Главное меню', resize_keyboard=True, one_time_keyboard=True)
